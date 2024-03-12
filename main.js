c208d6403699ec218f7fc1f3136d6465710e0c23
Aeden Manell <nthabelengaedenmanell@gmail.com>
2024-03-12 03:03:25 +0200

    added a leaderboard function

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-/*The following main.js file is for the dice game 
-which incorporates various key elements. It establishes clear rules 
-and mechanics, allowing users to interact with the game through 
-buttons that trigger corresponding actions. */
+/*
+The following javascript code handles the functionality of rolling dice, determining 
+the winner, updating win counts, and allowing the user to edit player 
+names. It also provides a restart option to reset the game.
+*/
 
-/* This function reloads the current webpage, 
-effectively refreshing the page and resetting its state.*/
+/* 
+This function reloads the current webpage, 
+effectively refreshing the page and resetting its state.
 
-/* The reload() method reloads the current document.
+The reload() method reloads the current document.
 The reload() method does the same as the reload 
-button in your browser.*/
+button in your browser.
+*/
 function restartDiceGame() {
   /* The location.reload() method is a built-in function 
   in JavaScript that allows you to reload the 
@@ -16,71 +19,88 @@ function restartDiceGame() {
   location.reload();
 }
 
+//These variables keep track of the number of wins for player 1 and player 2, respectively.
+let player1Winner = 0;
+let player2Winner = 0;
+
 //This function simulates a dice roll and displays the results on the dicegame.html page
 function rollTheDice() {
-    // Selects the dice images for player 1 and player 2
-    let dice1 = document.querySelector(".player1dice-img");
-    let dice2 = document.querySelector(".player2dice-img");
+  // Selects the dice images for player 1 and player 2
+  let dice1 = document.querySelector(".player1dice-img");
+  let dice2 = document.querySelector(".player2dice-img");
 
-    /* Selects the HTML element with the tag <h2> and class "results" in order
-    to display the results of the dice roll*/
-    let result = document.querySelector('h1');
-    
-    result.innerHTML = " ";
+  /* Selects the HTML element with the tag <h2> and class "results" in order
+  to display the results of the dice roll*/
+  let result = document.querySelector("h1");
 
-    // Generates random numbers for each player's dice roll, by calling the diceGenerator() function.
-    let player1Dice = diceGenerator();
-    let player2Dice = diceGenerator();
+  result.innerHTML = " ";
 
-    // Set the source attribute of "dice1" to display the corresponding dice image for player 1
-    dice1.setAttribute('src', 'diceimages/dice' + player1Dice + '.png');
-    // Set the source attribute of "dice2" to display the corresponding dice image for player 2
-    dice2.setAttribute('src', 'diceimages/dice' + player2Dice + '.png');
+  // Generates random numbers for each player's dice roll, by calling the diceGenerator() function.
+  let player1Dice = diceGenerator();
+  let player2Dice = diceGenerator();
 
-    // Determines the winner based on the dice rolls
-    if (player1Dice === player2Dice) {
-      result.innerHTML = "DRAW!";
-    } else if (player1Dice < player2Dice) {
-      result.innerHTML = (player2 + " WINS!"); // Assuming player names are stored in variables player1 and player2
-    } else {
-      result.innerHTML = (player1 + " WINS!"); // Assuming player names are stored in variables player1 and player2
-    }
+  // Set the source attribute of "dice1" to display the corresponding dice image for player 1
+  dice1.setAttribute("src", "diceimages/dice" + player1Dice + ".png");
+
+  // Set the source attribute of "dice2" to display the corresponding dice image for player 2
+  dice2.setAttribute("src", "diceimages/dice" + player2Dice + ".png");
+
+  // Determines the winner based on the dice rolls
+  if (player1Dice === player2Dice) {
+    result.innerHTML = "DRAW!";
+  } else if (player1Dice < player2Dice) {
+    result.innerHTML = player2 + " WINS!";
+    player2Winner++; // Increment player 2's win count
+  } else {
+    result.innerHTML = player1 + " WINS!";
+    player1Winner++; // Increment player 1's win count
+  }
+
+  // Update the leaderboard
+  updateLeaderboard();
 }
 
-/*Two variables, player1 and player2, are declared and assigned 
-to their initial values of "Player1" and "Player2".*/
 let player1 = "Player1";
 let player2 = "Player2";
 
-/*This function allows the user to update the names 
-of player1 and player2 by prompting them to enter new names using 
-the prompt() function.*/
+//This function is called when the user wants to edit the names of player 1 and player 2.
 function editNames() {
+  ///It prompts the user to enter new names for both players using the prompt() method.
+  player1 = prompt("Update Player 1 Name: ");
+  player2 = prompt("Update Player 2 Name: ");
 
-    player1 = prompt("Update Player 1 Name: ");
-    player2 = prompt("Update Player 2 Name: ");
+  /*
+If the entered names are valid (length greater than 0), it updates the 
+corresponding <p> elements with the new names using document.querySelector().
+If the entered names are not valid, it displays an alert message asking the 
+user to enter valid names.
+  */
+  if (player1.length < 1 || player2.length < 1) {
+    alert("Please enter valid name");
+  }
 
-    /*The code then performs error management by checking 
-    if the length of either player1 or player2 is less than 1 (empty string). 
-    If so, it displays an alert message asking the user to enter a valid name.*/
-    if(player1.length<1 || player2.length<1){
-        alert("Please enter valid name");
-    }
-
-/* the code uses the querySelector() method to select the HTML elements 
-with class names "Player1" and "Player2" (<p> elements) and 
-updates their innerHTML to display the new names entered by the user.*/
-    document.querySelector("p.Player1").innerHTML = player1;
-    document.querySelector("p.Player2").innerHTML = player2;
+  document.querySelector("p.Player1").innerHTML = player1;
+  document.querySelector("p.Player2").innerHTML = player2;
 }
 
-/* diceGenerator that generates a random number between 1 and 6, 
-simulating the roll of a dice. */
+//This function generates a random number between 1 and 6, representing a dice roll.
 function diceGenerator() {
+  /*It uses Math.floor() to round down the decimal to the nearest whole number ,
+  It adds 1 to the rounded number to get a random whole number between 1 and 6.*/
+  let randomNumber = Math.floor(Math.random() * 6) + 1;
 
-    //A random whole number between 1 and 6:
-    let randomNumber = Math.floor(Math.random() * 6) + 1;
-    
-        //Returns the generated random number.
-        return randomNumber;
-    }
+  //Returns the generated random number.
+  return randomNumber;
+}
+
+//This function updates the leaderboard with the current win counts for player 1 and player 2.
+function updateLeaderboard() {
+  //It selects the HTML element with the class "leaderboard" using document.querySelector().
+  let leaderboard = document.querySelector(".leaderboard");
+
+  /*It sets the innerHTML of the leaderboard element to a string containing 
+  the player names and their respective win counts.
+The win counts are obtained from the player1Winner and player2Winner variables.*/
+  leaderboard.innerHTML = `<div id="leaderboard-div"><p class="leaderboard-p">${player1} : ${player1Winner} wins</p>
+      <p class="leaderboard-p">${player2} : ${player2Winner} wins</p> </div>`;
+}
